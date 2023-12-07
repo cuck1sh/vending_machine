a43dab481ca0bd3113cc2ad2114ad448097aee35
Egor Kirin <egor.kirin20@gmail.com>
2023-12-08 00:34:36 +0600

    add custom exceptions package, modified payment method classes and modified commands

--- src/MoneyReceiver/BankCard.java
+++ src/MoneyReceiver/BankCard.java
@@ -1,8 +1,36 @@
 package MoneyReceiver;
 
+import java.util.Scanner;
+
 public class BankCard implements PaymentMethod{
+
+    private double cardBalance = 164.20;
+    static Scanner sc = new Scanner(System.in);
+
+
     @Override
     public void moneyInput() {
+        System.out.printf("Текущий баланс на карте: %.2f \n", cardBalance );
+        while (true) {
+            System.out.print("Сумма на пополнение: ");
+            try {
+                double sum = sc.nextInt();
+                if (sum >= 1 && sum <= cardBalance) {
 
+                } else {
+                    throw
+                }
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("Введите число");
+                moneyInput();
+            }
+
+
+
+        }
     }
+
+
+
 }
--- src/MoneyReceiver/Cash.java
+++ src/MoneyReceiver/Cash.java
@@ -3,6 +3,6 @@ package MoneyReceiver;
 public class Cash implements PaymentMethod{
     @Override
     public void moneyInput() {
-
+        System.out.println("");
     }
 }
